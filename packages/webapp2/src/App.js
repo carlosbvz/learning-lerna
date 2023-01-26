07027c534f784d24f0d785f56c40579cbd0bfd20
Carlos Benavides <carlosbvz@gmail.com>
2023-01-26 10:19:33 -0600

    feat: Updating title on webapp 2

--- packages/webapp2/src/App.js
+++ packages/webapp2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Webapp2
+          WebApp 2
         </p>
         <a
           className="App-link"
