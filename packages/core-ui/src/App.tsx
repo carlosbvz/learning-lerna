17b57f7057ea0b0ff6e99f5efe88086123902078
Carlos Benavides <carlosbvz@gmail.com>
2023-01-27 09:57:48 -0600

    feat(global): Adding typescript to projects

---
+++ packages/core-ui/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>{"Core UI"}</title>
+      </Helmet>
+      <div className="App">
+        <p>Core UI</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/webapp1/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <title>{"WebApp 1"}</title>
+      </Helmet>
+      <div className="App">
+        <p>Web App 1</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
